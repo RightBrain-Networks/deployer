e7297841b5eeb05ef723b07447a896bcc96fd0fa
Derek DeJonghe <derek.dejonghe@rightbrainnetworks.com>
2017-02-23 16:23:51 -0500

    logger add

---
+++ logger.py
@@ -0,0 +1,17 @@
+import logging
+# create logger
+logger = logging.getLogger('simple_example')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
