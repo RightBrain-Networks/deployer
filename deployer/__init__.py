e960f2d88502b5b1c0869cc125dbbc5f220eff8f
Kevin Lucas <kevin.lucas@rightbrainnetworks.com>
2018-04-09 16:26:30 -0400

    Use template body if bucket is null

--- deployer/__init__.py
+++ deployer/__init__.py
@@ -8,7 +8,7 @@ from deployer.logger import logging, logger, console_logger
 import ruamel.yaml
 
 
-__version__ = 'v0.3.8'
+__version__ = 'v0.3.9'
 
 def main():
     parser = argparse.ArgumentParser(description='Deploy CloudFormation Templates')
