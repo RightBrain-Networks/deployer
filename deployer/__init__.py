83e220eb924c35a041b65454e84664cbad6022cd
Joseph Manley <j@cloudsumu.com>
2019-07-25 17:02:48 -0400

    Implemented graph-based dependency checks and add --resolve-dependencies

--- deployer/__init__.py
+++ deployer/__init__.py
@@ -30,6 +30,7 @@ def main():
     parser.add_argument("-j", "--assume-valid", help="Assumes templates are valid and does not do upstream validation (good for preventing rate limiting)", action="store_true", dest="assume_valid", default=False)
     parser.add_argument("-D", "--debug", help="Sets logging level to DEBUG & enables traceback", action="store_true", dest="debug", default=False)
     parser.add_argument("-v", "--version", help='Print version number', action='store_true', dest='version')
+    parser.add_argument("-R","--resolve-dependencies", help='Includes dependencies when executing an action', dest="resolve", action='store_true', default=False)
 
     args = parser.parse_args()
 
@@ -77,68 +78,57 @@ def main():
         with open(args.config) as f:
             config = ruamel.yaml.safe_load(f)
 
-        effectedStacks = []
         stackQueue = []
         if not args.all:
-            # Add specified stack to queue
-            stackQueue.append(args.stack)
-        else:
-            # Add available stacks to queue
-            stackQueue = get_deployableStacks(config, effectedStacks)
-
-        #While there are stacks to be created
-        while(len(stackQueue) > 0):
-            # Create or update all Environments
-            for stack in stackQueue:
-                if stack != 'global' and (args.all or stack == args.stack):
-                    if args.sync:
-                        s3_sync(args.profile, args.config, stack, args.assume_valid)
-                    logger.info("Running " + str(args.execute) + " on stack: " + stack)
-                    env_stack = Stack(args.profile, args.config, stack, args.rollback, args.events, params)
-                    if args.execute == 'create':
-                        try:
-                            env_stack.create()
-                        except ClientError as e:
-                            if not args.all:
-                                raise e
-                            elif e.response['Error']['Code'] == 'AlreadyExistsException':
-                                logger.info("Stack, " + stack + ", already exists.")
-                            else:
-                                raise e
-                    elif args.execute == 'update':
-                        env_stack.update()
-                    elif args.execute == 'delete':
-                        env_stack.delete_stack()
-                    elif args.execute == 'upsert':
-                        env_stack.update() if env_stack.check_stack_exists() else env_stack.create()
-                    elif args.execute == 'describe':
-                        print(json.dumps(env_stack.describe(),
-                                        sort_keys=True,
-                                        indent=4,
-                                        separators=(',', ': '),
-                                        default=lambda x: x.isoformat()))
-                    elif args.execute == 'change':
-                        env_stack.get_change_set(args.change_set_name, args.change_set_description, 'UPDATE')
-                    elif args.sync or args.execute == 'sync':
-                        s3_sync(args.profile, args.config, args.stack, args.assume_valid)
-                    effectedStacks.append(stack)
-            if args.all:
-                #Generate check for new deployable stacks
-                stackQueue = get_deployableStacks(config, effectedStacks)
+            if args.resolve:
+                # Add stack and it's dependencies to queue
+                stackQueue = find_deploy_path(config, args.stack, stackQueue)
             else:
-                stackQueue.remove(stack)
+                # Add specified stack to queue
+                stackQueue = [args.stack]
+        else:
+            for stack in config.items():
+                if stack[0] != "global":
+                    stackQueue = find_deploy_path(config, stack[0], stackQueue)
 
-        #Check for any remaining stacks
-        if args.all:
-            remainingStacks = []
-            for item in config.items():
-                if item[0] not in effectedStacks and item[0] != "global":
-                    remainingStacks.append(item[0])
-            #Give error message if there were any circular dependencies
-            if(len(remainingStacks) > 0):
-                logger.error(str(len(remainingStacks)) + " stack(s) had circular dependencies:")
-                for stack in remainingStacks:
-                    logger.error("      " + stack)
+        print(stackQueue)
+        input()
+
+        # Create or update all Environments
+        for stack in stackQueue:
+            if stack != 'global' and (args.all or stack == args.stack):
+                if args.sync:
+                    s3_sync(args.profile, args.config, stack, args.assume_valid)
+                logger.info("Running " + str(args.execute) + " on stack: " + stack)
+                env_stack = Stack(args.profile, args.config, stack, args.rollback, args.events, params)
+                if args.execute == 'create':
+                    try:
+                        env_stack.create()
+                    except ClientError as e:
+                        if not args.all:
+                            raise e
+                        elif e.response['Error']['Code'] == 'AlreadyExistsException':
+                            logger.info("Stack, " + stack + ", already exists.")
+                        else:
+                            raise e
+                elif args.execute == 'update':
+                    env_stack.update()
+                elif args.execute == 'delete':
+                    env_stack.delete_stack()
+                elif args.execute == 'upsert':
+                    env_stack.update() if env_stack.check_stack_exists() else env_stack.create()
+                elif args.execute == 'describe':
+                    print(json.dumps(env_stack.describe(),
+                                    sort_keys=True,
+                                    indent=4,
+                                    separators=(',', ': '),
+                                    default=lambda x: x.isoformat()))
+                elif args.execute == 'change':
+                    env_stack.get_change_set(args.change_set_name, args.change_set_description, 'UPDATE')
+                elif args.sync or args.execute == 'sync':
+                    s3_sync(args.profile, args.config, args.stack, args.assume_valid)
+
+
 
     except (Exception) as e:
         logger.error(e)
@@ -152,51 +142,35 @@ def main():
             except:
                 pass
 
-def find_deploy_path(stackConfig, stack, deployed = []):
+def find_deploy_path(stackConfig, checkStack, resolved = []):
     #Generate depedency graph
     graph = {}
     for stack in stackConfig.items():
         if stack[0] != "global":
             edges = []
-            for param in stackConfig[1]['lookup_parameters']:
-                if param['Stack'] not in edges:
-                    edges.append(param['Stack'])
+            if 'lookup_parameters' in stack[1]:
+                for param in stack[1]['lookup_parameters']:
+                    edge = stack[1]['lookup_parameters'][param]
+                    if edge['Stack'] not in edges:
+                        edges.append(edge['Stack'])
             graph[stack[0]] = edges
-    result = []
-    resolve_dependency(graph, stack, result)
+
+    #Find dependency order
+    resolve_dependency(graph, checkStack, resolved)
+    return resolved
 
 def resolve_dependency(graph, node, resolved, seen = []):
     seen.append(node)
     for edge in graph[node]:
         if edge not in resolved:
+            #If node has already been seen, it's a circular dependency
             if edge in seen:
                 raise Exception("Circular dependency detected between stacks %s and %s." % (node, edge))
+            #Check edge for dependencies
             resolve_dependency(graph, edge, resolved, seen)
-    resolved.append(node)
+    if node not in resolved:
+        resolved.append(node)
 
-
-def get_deployableStacks(config, effectedStacks):
-    deployableStacks = [] #Output
-    checkQueue = []
-
-    #Add any stacks that were not created to queue
-    for item in config.items():
-        if item[0] not in effectedStacks and item[0] != "global":
-            checkQueue.append(item[0])
-
-    #Check stacks for dependencies
-    for stack in checkQueue:
-        haveAllDependency = True
-        for item in config.items():
-            if item[0] == stack:
-                if 'lookup_parameters' in item[1]:
-                    for parameter in item[1]['lookup_parameters']:
-                        if item[1]['lookup_parameters'][parameter]['Stack'] not in effectedStacks:
-                            haveAllDependency = False
-                            break
-        if haveAllDependency:
-            deployableStacks.append(stack)
-    return deployableStacks
 
 
 if __name__ == '__main__':
